8413d4fdc3c5c9bda8fa287acdc1a83580a60384
Weyller <weyller@gmail.com>
2017-07-03 01:21:04 -0400

    about class brain et singleton

---
+++ Simone/SimoneBrain.swift
@@ -0,0 +1,16 @@
+//
+//  SimoneBrain.swift
+//  Simone
+//
+//  Created by Local Admin on 2017-07-03.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import Foundation
+
+class SimonBrain{
+    
+    
+    
+    
+}
---
+++ Simone/SingletonShared.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonShared.swift
+//  Simone
+//
+//  Created by Local Admin on 2017-07-03.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import Foundation
+
+class SingletonShared{
+    
+    static let SingletonSharedInstance = SingletonShared()
+    
+    
+}
+//=========================================================
