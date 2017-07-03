7c3bb64e31bb4c4ab29c3813902a223693d0b268
Weyller <weyller@gmail.com>
2017-07-03 02:18:36 -0400

    array of buttons random buttons

--- Simone/GameController.swift
+++ Simone/GameController.swift
@@ -25,13 +25,39 @@ class GameController: UIViewController {
     @IBOutlet weak var scoreKeeper: UILabel!
     //----------------------------------
    
+    var arrOfButtons: [UIButton]!
+    var arrOfRandomButtons: [UIButton] = []
+    
+    //----------------------------------
+    
+    let simoneBrain = SimonBrain()
+    
     //==================================
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+         arrOfButtons = [but1, but2, but3, but4, but5, but6, but7, but8, but9]
+        
+        //---------------
+        
+        addRandomButtonToArray()
+        //----------------
+        
+        
     }
-
+    //------------------------------
+    
+    func addRandomButtonToArray()
+    {
+        let randomIndex = simoneBrain.getRandomNumber(from: 0, to: arrOfButtons.count-1)
+        arrOfRandomButtons.append(arrOfButtons[randomIndex])
+       
+        print(arrOfRandomButtons)
+    
+    
+    }
+    
+    //------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Simone/SimoneBrain.swift
+++ Simone/SimoneBrain.swift
@@ -8,9 +8,18 @@
 
 import Foundation
 
+//=============================================
 class SimonBrain{
     
-    
+    //----------------------------------------
+    func getRandomNumber(from f: Int, to t: Int) -> Int{
+        
+        let from =  UInt32(f)
+        let to = UInt32(t)
+        let randomNumber = arc4random_uniform(to - from + 1) + from
+        return Int(randomNumber)
+    }
     
     
 }
+//======================================
